90bcefc7dedd3a89340d82b741f0709b17cb49dd
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-08-21 16:38:43 -0600

    adding files and additional vars

--- liri.js
+++ liri.js
@@ -8,3 +8,24 @@ var fs = require("fs");
 var spotify = new Spotify({
   id: keys.spotify.id,
   secret: keys.spotify.secret,
+});
+// var defaultSong = require("The Sign");
+var defaultMovie = "Mr. Nobody";
+// var spotify = new Spotify(keys.spotify);
+
+/**
+ * Name of the venue
+Venue location
+Date of the Event (use moment to format this as "MM/DD/YYYY")
+ */
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action) {
+  case "concert-this":
+    getBands(value)
+    break;
+  case "spotify-this-song":
+    //If user has not specified a song , use default
+    // if (value === "") {
+    //   value = defaultSong;
